27fa9d9d82646692ff27417ee3e6ceab707f1765
brhgreen <benedictgreen@gmail.com>
2018-02-23 14:45:05 +0000

    a commit

--- src/App.js
+++ src/App.js
@@ -6,10 +6,17 @@ class App extends Component {
   render() {
     return (
       <div className="wrapper">
-      <header className="header">
-      <h3>How to help</h3>
-      <i class="icon icon-cross" />
-      </header>
+        <header className="header">
+          <img src="../cr.png" />
+          <h3>Here's how you can help</h3>
+          <i className="icon icon-cross" />
+        </header>
+      <div className="page page-1">
+        <p>
+          some text
+        </p>
+        <button>Donate</button>
+      </div>
       </div>
     );
   }
