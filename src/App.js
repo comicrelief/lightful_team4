36ae500d1ac661692ceb762762e9c74a0fed9770
brhgreen <benedictgreen@gmail.com>
2018-02-23 14:13:25 +0000

    sass running. spectre installed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './index.css';
 
 class App extends Component {
   render() {
     return (
       <div className="wrapper">
-        
+        <div className="page page-1">
+          
+        </div>
       </div>
     );
   }
